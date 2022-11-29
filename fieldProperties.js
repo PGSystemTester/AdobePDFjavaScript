2dcb07a48c3a3e256203ed6f438af26d9b3f7f02
pgSystemTester <7731989+PGSystemTester@users.noreply.github.com>
2022-11-28 20:45:59 -0800

    Update fieldProperties.js
    more properties

--- fieldProperties.js
+++ fieldProperties.js
@@ -4,16 +4,23 @@ for (var fieldNumber = 0; fieldNumber < this.numFields ; fieldNumber++){
 var d = this.getField(this.getNthFieldName(fieldNumber));
 console.println('Name: ' + d.name 
      +'\n Type:' + d.type
-     + '\n Required:' + d.required
+     + '\n required:' + d.required
      + '\n value:' + d.value
-     + '\n Page:' + d.page
+     + '\n valueAsString:' + d.valueAsString 
+     + '\n defaultValue :' + d.defaultValue 
+     + '\n page:' + d.page
      + '\n calcOrderIndex :' + d.calcOrderIndex
      + '\n submitName:' + d.submitName
-     + '\n UserName :' + d.userName 
-     + '\n Display :' + d.display 
-     + '\n UserName :' + d.userName 
-     + '\n UserName :' + d.userName
-     + '\n UserName :' + d.userName
-     + '\n UserName :' + d.userName
+     + '\n userName :' + d.userName 
+     + '\n Display :' + d.display
+     + '\n charLimit:' + d.charLimit
+     + '\n rect  :' + d.rect
+/*
+     
+     + '\n buttonGetCaption() :' + d.buttonGetCaption()
+     + '\n userName :' + d.userName 
+     + '\n userName :' + d.userName 
+     + '\n userName :' + d.userName 
+*/
 );
 }
